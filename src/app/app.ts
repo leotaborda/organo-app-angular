8aa5ef0fdf7bf65f1c0254d4d3c4fd425ecba8a3
leotaborda <leo.taboorda@gmail.com>
2025-08-28 09:44:02 -0300

    feat(app): import book component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
+import { Book } from "./components/book/book";
 
 @Component({
   selector: 'app-root',
-  imports: [ Header, Footer],
+  imports: [Header, Footer, Book],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
