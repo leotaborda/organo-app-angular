ab9bf3c7a83083949646758dc82ab899332ffb48
leotaborda <leo.taboorda@gmail.com>
2025-08-28 10:18:28 -0300

    feat(component): added favorite button

--- src/app/components/book/book.ts
+++ src/app/components/book/book.ts
@@ -4,13 +4,17 @@ import { Component } from '@angular/core';
   selector: 'app-book',
   imports: [],
   templateUrl: './book.html',
-  styleUrl: './book.css'
+  styleUrl: './book.css',
 })
 export class Book {
-
   book = {
-    titulo: "As ondas",
-    autoria: "Virginia Woolf",
-    image: "https://m.media-amazon.com/images/I/81D2d6LqZdS._UF1000,1000_QL80_.jpg"
+    titulo: 'As ondas',
+    autoria: 'Virginia Woolf',
+    image: 'https://m.media-amazon.com/images/I/81D2d6LqZdS._UF1000,1000_QL80_.jpg',
+    favorite: false,
+  };
+
+  alterarFavorito() {
+    this.book.favorite = !this.book.favorite
   }
 }
