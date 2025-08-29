9034f2283e6d3bfd17d99be6b4fdb9f38935397b
leotaborda <leo.taboorda@gmail.com>
2025-08-29 11:18:13 -0300

    feat(component): update genre component

--- src/app/components/genre/genre.ts
+++ src/app/components/genre/genre.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { Book } from '../book/book';
+import { Books } from '../../mock-books';
 
 @Component({
   selector: 'app-genre',
-  imports: [],
+  imports: [Book],
   templateUrl: './genre.html',
   styleUrl: './genre.css'
 })
 export class Genre {
+  book = Books[0]
 
 }
