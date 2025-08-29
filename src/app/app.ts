3a938b1daf05f5397ab50af63e16365cd59bbe98
leotaborda <leo.taboorda@gmail.com>
2025-08-29 11:19:19 -0300

    feat(app): added new component of genre

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
 //import { Book } from "./components/book/book";
 import { BooksList } from "./components/books-list/books-list";
+import { Genre } from "./components/genre/genre";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer, BooksList],
+  imports: [Header, Footer, BooksList, Genre],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
