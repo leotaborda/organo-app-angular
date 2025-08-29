763236a4873f495800e823426b0922380d4bc6b1
leotaborda <leo.taboorda@gmail.com>
2025-08-29 11:06:37 -0300

    feat(components): created new component

---
+++ src/app/components/genre/genre.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-genre',
+  imports: [],
+  templateUrl: './genre.html',
+  styleUrl: './genre.css'
+})
+export class Genre {
+
+}
