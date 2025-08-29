942597c440dfd5f4093635a750925a29cc0b669b
leotaborda <leo.taboorda@gmail.com>
2025-08-29 11:03:24 -0300

    feat(app): remove Book and added BooksList

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
-import { Book } from "./components/book/book";
+//import { Book } from "./components/book/book";
+import { BooksList } from "./components/books-list/books-list";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer, Book],
+  imports: [Header, Footer, BooksList],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
