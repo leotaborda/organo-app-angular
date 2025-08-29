3f1b85d236e3b29bf2186b2a52f9605f5fb5c55d
leotaborda <leo.taboorda@gmail.com>
2025-08-29 10:29:14 -0300

    feat(component): added interface and genre in book component

--- src/app/components/book/book.ts
+++ src/app/components/book/book.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IBook } from './book.component';
 
 @Component({
   selector: 'app-book',
@@ -7,11 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './book.css',
 })
 export class Book {
-  book = {
+  book: IBook = {
     title: 'As ondas',
     author: 'Virginia Woolf',
     image: 'https://m.media-amazon.com/images/I/81D2d6LqZdS._UF1000,1000_QL80_.jpg',
     favorite: false,
+    genre: "Ficção"
   };
 
   alterarFavorito() {
